57f909e5a4713ef30aa3ca0882d5da98162e53e3
Akira Kobori <private.beats@gmail.com>
2020-04-10 19:22:07 +0900

    Packaging

--- setup.py
+++ setup.py
@@ -10,16 +10,13 @@ def get_requirements():
 
 setup(
     name="due_deligence",
-    version='1.2',
+    version='1.3',
     description='EDINETから有価証券報告書を取得して企業の割安度を判定します。',
     author='Kobori Akira',
-    # maintainer='',
-    # maintainer_email='',
     author_email='private.beats@gmail.com',
     url='https://github.com/koboriakira/due_deligence',
     packages=find_packages(),
-    requires=get_requirements(),
-    # requires=open('./requirements.txt').read().splitlines(),
+    install_requires=open('requirements.txt').read().splitlines(),
     license='MIT',
     entry_points="""
       # -*- Entry points: -*-
