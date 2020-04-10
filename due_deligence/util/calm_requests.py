59caaea1964ed8008e1d1b38a32c1583650030de
Akira Kobori <private.beats@gmail.com>
2020-04-11 02:00:09 +0900

    remove session

--- due_deligence/util/calm_requests.py
+++ due_deligence/util/calm_requests.py
@@ -1,12 +1,10 @@
 import requests
 import time
 
-session = requests.Session()
-
 
 def get(url: str):
     """
     必ず1秒待つことでサーバ負荷を抑える
     """
     time.sleep(1)
-    return session.get(url, stream=True)
+    return requests.get(url, stream=True)
