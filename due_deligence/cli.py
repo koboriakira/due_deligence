2883a57a0a0aee53e48fb1bc522160b844aeec19
Akira Kobori <private.beats@gmail.com>
2020-04-12 18:10:06 +0900

    自己資本比率、株価のフィルターを追加

--- due_deligence/cli.py
+++ due_deligence/cli.py
@@ -8,6 +8,8 @@ from typing import List, Tuple
 
 from due_deligence.filtering.result_filter import ResultFilter
 from due_deligence.filtering.result_underpriced_filter import ResultUnderpricedFilter
+from due_deligence.filtering.result_capital_ratio_filter import ResultCapitalRatioFilter
+from due_deligence.filtering.result_stock_price_filter import ResultStockPriceFilter
 from due_deligence.controller import dd_controller
 from due_deligence.myconfig import myconfig, inject_config
 
@@ -24,6 +26,10 @@ def main():
         '--format', help='結果の出力形式を指定します。CSV出力、json出力から選択できます。\n指定しない場合は標準出力されます。', choices=['csv', 'json'], default='screen')
     parser.add_argument(
         '--underpriced', help='指定した割安度より割安な企業に絞り込みます', type=int, default=-1)
+    parser.add_argument(
+        '--capital', help='自己資本比率が指定した割合より高い企業に絞り込みます', type=int, default=-1)
+    parser.add_argument(
+        '--price', help='指定した値より安い株価の企業に絞り込みます', type=int, default=-1)
     parser.add_argument('--debug', help='開発者モード', type=bool, default=False)
     args = parser.parse_args()
 
@@ -39,7 +45,7 @@ def main():
 
     # 処理の実行
     target_dates = extract_date(args.date)
-    filters = prepare_filters(args.underpriced)
+    filters = prepare_filters(args.underpriced, args.capital, args.price)
     try:
         controller = dd_controller.DDController(
             from_date=target_dates[0], end_date=target_dates[1], filters=filters)
@@ -67,8 +73,12 @@ def extract_date(val: str) -> Tuple[date, date]:
     return from_date, end_date
 
 
-def prepare_filters(underpriced: int) -> List[ResultFilter]:
+def prepare_filters(underpriced: int, capital: int, price: int) -> List[ResultFilter]:
     filters = []
     if underpriced > 0:
         filters.append(ResultUnderpricedFilter(underpriced))
+    if capital > 0:
+        filters.append(ResultCapitalRatioFilter(capital))
+    if price > 0:
+        filters.append(ResultStockPriceFilter(price))
     return filters
