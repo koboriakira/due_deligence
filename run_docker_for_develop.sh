5ebd565c2020be3bac6eb6aecdb831421dc6c87d
Akira Kobori <private.beats@gmail.com>
2020-04-13 02:44:17 +0900

    Dockerコンテナのrunを調整

--- run_docker_for_develop.sh
+++ run_docker_for_develop.sh
@@ -1,11 +1,8 @@
 docker run -it --rm \
   -v $(pwd)/due_deligence:/work/due_deligence \
   -v $(pwd)/tests:/work/tests \
-  -v $(pwd)/logs:/work/logs \
-  -v $(pwd)/sample:/work/sample \
   -v $(pwd)/setup.py:/work/setup.py \
   -v $(pwd)/requirements.txt:/work/requirements.txt \
-  -v $(pwd)/.duedeli_request_cache:/work/.duedeli_request_cache \
   -v ~/.pypirc:/root/.pypirc \
   --name due_deligence \
   due_deligence_image
